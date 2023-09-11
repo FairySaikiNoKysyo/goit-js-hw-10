57da780118ddf76fe584094b23863137a7cfe1ef
FairySaikiNoKysyo <michaelevgeniymyers@gmail.com>
2023-09-11 19:05:21 +0300

    add

---
+++ src/cat-api.js
@@ -0,0 +1,32 @@
+import Notiflix from 'notiflix';
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY = 'live_k56NhF6Fgxd3eB7Cdejwig9QtWhpdwAQAB2B91u4etEu1xiUdYyK77JNLUEqVxxB';
+
+export function fetchBreeds() {
+
+    const END_POINT = '/breeds';    
+    return fetch(`${BASE_URL}${END_POINT}?${API_KEY}`)
+    .then((resp) => {
+        if (!resp.ok) {           
+            throw new Error(Notiflix.Notify.failure(`${textError.textContent}`));            
+        }
+        return resp.json();
+    })
+};
+
+export function fetchCatByBreed(breedId) {
+    const END_POINT = '/images/search';
+  const params = new URLSearchParams({
+    api_key: "live_k56NhF6Fgxd3eB7Cdejwig9QtWhpdwAQAB2B91u4etEu1xiUdYyK77JNLUEqVxxB",
+    breed_ids: breedId
+  });
+    
+   return fetch(`${BASE_URL}${END_POINT}?${params}`)
+    .then((resp) => {
+        if (!resp.ok) {            
+            throw new Error(Notiflix.Notify.failure(`${textError.textContent}`));
+        }
+        return resp.json();
+    })
+};
--- src/index.js
+++ src/index.js
@@ -1,40 +1,58 @@
-import axios from 'axios';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-import{ fetchBreeds,fetchCatByBreed } from "./partials/cat.api.js";
+const breedSelect = document.querySelector('.breed-select');
+const textLoader = document.querySelector('.loader');
+const textError = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_UoZkO9lLn5KL3Q08dfC36N7KEsxgDZs70d3w65oNsbf4uVSIQKOigih0ldPo7yek';
-
-
-
-
-const elements = {
-    select:document.querySelector( '.js-breed-select'),
-   catInfo: document.querySelector('.js-cat-info'),
-   loading :document.querySelector('.js-loader'),
-   errorText:document.querySelector('.js-error'),
-}
-
-
-
-elements.loading.style.display = 'none';
+textError.style.display = 'none';
 
 window.addEventListener('DOMContentLoaded', () => {
-    elements.select.insertAdjacentHTML("afterbegin", `<option>Select_breed</option>`);
-    // elements.select.style.display = 'none';
-   
-    fetchBreeds()    
+  breedSelect.insertAdjacentHTML('afterbegin', `<option>Select_breed</option>`);
+  breedSelect.style.display = 'none';
+
+  fetchBreeds()
     .then(data => {
-        
-        elements.select.style.display = 'block';
-        elements.loading.style.display = 'none';
-        let option = data.map(({ id, name }) => `<option value="${id}">${name}</option>`).join('');
-        elements.select.insertAdjacentHTML("beforeend", option);
-    
+      breedSelect.style.display = 'block';
+      textLoader.style.display = 'none';
+      let option = data
+        .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+        .join('');
+      breedSelect.insertAdjacentHTML('beforeend', option);
     })
-    .catch(error =>            
-        elements.loading.style.display = 'none'            
-    );   
-    
-   
+    .catch(error => (textLoader.style.display = 'none'));
 });
+
+breedSelect.addEventListener('change', handlerChange);
+
+function handlerChange() {
+  const selectedBreedId = breedSelect.value;
+  textLoader.style.display = 'block';
+  textError.style.display = 'none';
+  catInfo.style.display = 'none';
+
+  fetchCatByBreed(selectedBreedId)
+    .then(data => {
+      if (data[0] === undefined) {
+        textError.style.display = 'block';
+        textError.style.color = 'tomato';
+        textLoader.style.display = 'none';
+      } else {
+        textLoader.style.display = 'none';
+        catInfo.style.display = 'block';
+
+        const { url, width, height, breeds } = data[0];
+        catInfo.innerHTML = `<img class="cat" src="${url}" alt="${
+          data[0].breeds[0].name
+        }" width="${width * 0.3}" height="${height * 0.3}"/>
+            <h2>${data[0].breeds[0].name}</h2>
+            <p>${data[0].breeds[0].description}</p>
+            <h3>Temperament:${data[0].breeds[0].temperament}</h3>`;
+      }
+    })
+
+    .catch(error => {
+      console.log(error);
+      textLoader.style.display = 'none';
+    });
+}
